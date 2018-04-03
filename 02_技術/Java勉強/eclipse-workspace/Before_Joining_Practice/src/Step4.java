d799e966e037cca30320826146c823e86c2736c3
nekonisi <konishi2175@gmail.com>
2018-04-03 11:17:02 +0900

    eclipseのワークスペースをコミット

--- 02_技術/Java勉強/eclipse-workspace/Before_Joining_Practice/src/Step4.java
+++ 02_技術/Java勉強/eclipse-workspace/Before_Joining_Practice/src/Step4.java
@@ -1,13 +1,17 @@
-
 import java.util.Scanner;
 
 public class Step4 {
 	public static void main() {
 
-		// 最初のメッセージを表示するよ！
-		ShowMessage(0);
+		try {
+			// 最初のメッセージを表示するよ！
+			ShowMessage(0);
 
-		// キーボードからの読込
+			// キーボードからの読込
+		} catch (Exception e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
 		@SuppressWarnings("resource")
 		Scanner scan = new Scanner(System.in);
 		String inputString = scan.next();
@@ -39,12 +43,12 @@ public class Step4 {
 
 		// 1文字目が-だったら桁数を一つ少なく
 		if( inputCharArray[0]=='-'){
-		digitLen--;
+			digitLen--;
 		}
 
 		System.out.println("＊＊□数字チェック結果□＊＊");
 		System.out.println("□ＡＬＬ ＮＵＭＢＥＲ！！\r\n");
- 
+
 		System.out.print("入力した文字列は" + inputInt);
 		System.out.print("で桁数は" + digitLen + "です。\r\n");
 	}
@@ -52,18 +56,18 @@ public class Step4 {
 	/* メッセージを表示するだけのクソクラス*/
 	private static void ShowMessage(int num){
 		switch (num){
-			case 0: // 最初のメッセージのやつ
-				System.out.println("＊＊□数字チェックプログラム□＊＊");
-				System.out.println("＊＊□整数を入力して下さい□□＊＊");
-				break;
+		case 0: // 最初のメッセージのやつ
+			System.out.println("＊＊□数字チェックプログラム□＊＊");
+			System.out.println("＊＊□整数を入力して下さい□□＊＊");
+			break;
 
-			case 1: // 桁数がおかしいとき
-				System.out.println("＊＊□入力桁数が不正です！□□＊＊\r\n");
-				break;
+		case 1: // 桁数がおかしいとき
+			System.out.println("＊＊□入力桁数が不正です！□□＊＊\r\n");
+			break;
 
-			case 2: // 文字の種類がおかしいとき
-				System.out.println("＊＊□入力した文字が不正です！□□＊＊\r\n");
-				break;
+		case 2: // 文字の種類がおかしいとき
+			System.out.println("＊＊□入力した文字が不正です！□□＊＊\r\n");
+			break;
 		}
 	}
 
@@ -89,22 +93,22 @@ public class Step4 {
 	/* 文字の種類をチェックする
 		 仕様見る限り、半角数字だけOKにすればよくね？*/
 	private static boolean checkCharKind(char inputChar[]){
-		
+
 		// match関数で使用する正規表現のパターン
 		String pattern="";
-			for (int charCount = 0; charCount < inputChar.length; charCount++){
-				if (charCount==0) {
-					// 1文字目
-					pattern="[-|+|0-9]";
-				} else {
-					// 1文字よりあと
-					pattern="[0-9]";
-				}
-				if (String.valueOf(inputChar[charCount]).matches(pattern)) {
-				} else {
-					return false;
-				}
+		for (int charCount = 0; charCount < inputChar.length; charCount++){
+			if (charCount==0) {
+				// 1文字目
+				pattern="[-|+|0-9]";
+			} else {
+				// 1文字よりあと
+				pattern="[0-9]";
 			}
+			if (String.valueOf(inputChar[charCount]).matches(pattern)) {
+			} else {
+				return false;
+			}
+		}
 		return true;
 	}
 }
